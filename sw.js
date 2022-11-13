c7dbd4ca9fe124bea436127c32340bfad777a200
dong1111yyyy <2060214611@qq.com>
2022-11-13 08:27:05 +0800

    1
    1

---
+++ sw.js
@@ -0,0 +1,28 @@
+/***
+ * 初始化安装
+ */
+self.addEventListener('install', () => {
+  // 1、优先缓存 js/css/html  2、在缓存 图片、字体、媒体
+});
+
+/**
+ * 激活生效
+ */
+self.addEventListener('activate', () => {
+
+});
+
+/**
+ * 拦截、缓存、
+ */
+self.addEventListener('fetch', () => {
+
+});
+
+/**
+ * 通信：页面和sw通过postMessage和监听message来通信
+ * 缓存管理
+ */
+self.addEventListener('message', () => {
+
+});
